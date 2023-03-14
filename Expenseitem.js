6681aa8a1dbc3f19cc20f3b7f17926ea534f6f72
Ganesh Unakal <122563639+ganesh-unakal@users.noreply.github.com>
2023-03-14 19:15:55 +0530

    basic of react Expenseitem

---
+++ Expenseitem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return <div>
+        <h2>Expense Item</h2>
+        <div>Food Rs 10
+        <div>Petrol Rs 100</div>
+        <div> Movies Rs 200</div>
+        </div>
+        </div>
+}
+// we need to export thz function otherwise its only 
+//usable inside of file & thats not helpful us
+export default ExpenseItem;
