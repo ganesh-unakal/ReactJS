2a3d33c2911c383b4a0ca9a4cd1c3e89d92aa3e5
Ganesh Unakal <122563639+ganesh-unakal@users.noreply.github.com>
2023-03-15 18:54:51 +0530

    passingDataUsingProps9th

--- Expenseitem.js
+++ Expenseitem.js
@@ -1,11 +1,16 @@
-function ExpenseItem(){
-    return <div>
-        <h2>Expense Item</h2>
-        <div>Food Rs 10
-        <div>Petrol Rs 100</div>
-        <div> Movies Rs 200</div>
-        </div>
-        </div>
+import './Expenseitem.css';
+
+
+function ExpenseItem(props){
+//now we acces the the outside of data
+    return <div className='expense-item'>
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+            <div className='expense-item__price'>{props.location}</div>
+            </div>
+    </div>
 }
 // we need to export thz function otherwise its only 
 //usable inside of file & thats not helpful us
