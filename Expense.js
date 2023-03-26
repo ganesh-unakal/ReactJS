d60456071d18151119374f1fa1b8f2a90a8ae140
Ganesh Unakal <122563639+ganesh-unakal@users.noreply.github.com>
2023-03-26 20:10:43 +0530

    conditional rendring 12

--- Expense.js
+++ Expense.js
@@ -1,43 +1,51 @@
-import React,{useState} from "react";
-import Expenseitem from "./Expenseitem";
+import React, { useState } from "react";
+
 import Card from "../UI/Card";
 import "./Expense.css";
 import ExpenseFilter from "./ExpensesFilter";
-import ExpenseItem from "./Expenseitem";
+import ExpensesList from "./ExpensesList";
 //import Expense from './Expense'
 
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-const Expenses=(props)=> {
-  const [filteredYear, setFilteredYear]=useState("2020")
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-  const filterChangeHandler = selectedYear => {
-    setFilteredYear(selectedYear)
-  }
-
-  const filteredExpenses= props.items.filter(expense=>{
-    return expense.date.getFullYear().toString()=== filteredYear;
-  })
-
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
-    <Card className="expenses">
-
-       <ExpenseFilter 
-       selected = {filteredYear}
-       onChangeFilter = {filterChangeHandler} />
-
-    {filteredExpenses.map((expense,index)=>( //props items means array of items which is in App.js file 
-      <ExpenseItem
-      key={expense.id}
-      title={expense.title} //expesne is used to extract the title
-      amount={expense.amount}
-      date={expense.date}
-      />
-    ))}     
-    </Card>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+          <ExpensesList items={filteredExpenses} />
+      </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
+
+/*{filteredExpenses.length === 0 ? (
+  <p>No Expense Found</p>
+) : (
+  filteredExpenses.map(
+    (
+      expense,
+      index //props items means array of items which is in App.js file
+    ) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title} //expesne is used to extract the title
+        amount={expense.amount}
+        date={expense.date}
+      />
+    )
+  )
+)}*/
